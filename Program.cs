74231e419a5b207c464765e7a2ee5116b08ac3e5
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2021-01-01 19:46:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace practicalLINQ
+{
+    
+    public interface Name
+    {
+        
+        string First { get; }
+        string Middle { get; }
+        
+        string Last { get; }
+    }
+
+    public  class OrderBy1
+    {
+        
+        public static IEnumerable<Name> SortNames(IEnumerable<Name> names)
+        {
+            
+            return names.OrderByDescending(_ => _.Last);
+            
+        }
+        
+    }
+    public  class ThenBy1
+    {
+        
+        public  IEnumerable<Name> SortNames(IEnumerable<Name> names)
+        {
+            
+            return names.OrderBy(_ => _.Last).ThenBy(_ => _.First).ThenBy(_ => _.Middle);
+           
+        }
+    }
+    public class Program{
+        public static void Main(){
+            OrderBy1 o=new OrderBy1();
+            ThenBy1 t=new ThenBy1();
+            //o.SortNames(IEnumerator<Name> GetEnumerator(););
+            o.GetType();
+            t.GetType();
+        }
+    }
+    public static class MethodSyntax1
+    {
+        public static IEnumerable<string> FilterAndSort(IEnumerable<string> inValues, string pattern)
+        {
+            return inValues
+                
+                .Where(_ => _.Contains(pattern))
+                .OrderBy(_ => _);
+        }
+    }
+}
